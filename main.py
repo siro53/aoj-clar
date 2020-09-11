54e22e0e74c893f082455f9018ee3401778628c6
siro53 <shusuke.shirotake@me.com>
2020-09-12 00:52:20 +0900

    コメントが更新されても取得できるようになった(多分)

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pprint
 import config
 import time
 import json
-import copy
 import sys
+import string
 
 
 def login(uid, upass):
@@ -46,23 +46,100 @@ def get_thread_list(arena_id, page, size):
     return response.json()
 
 
-def make_text(dic):
-    return {
-        'text': '新しいスレッドが追加されました！確認してください。\nproblemId: {0}\n userId: {1}\npolicy: {2}\ntype: {3}\ntitle: {4}\nhttps://onlinejudge.u-aizu.ac.jp/beta/room.html#WUPC2020/board'.format(dic['problemId'], dic['userId'], dic['policy'], dic['type'], dic['title'])
-    }
+def get_messages_on_thread(arena_id, problem_id):
+    url = 'https://judgeapi.u-aizu.ac.jp/threads/arenas/{0}/problems/{1}'.format(
+        arena_id, problem_id)
+    response = requests.get(url)
+
+    return response.json()
+
+
+def make_text_about_thread(dic):
+    visible = ''
+    if dic['visible'] == 1:
+        visible = 'public'
+    else:
+        visible = 'private'
+    url = 'https://onlinejudge.u-aizu.ac.jp/beta/room.html#' + config.ARENA_ID + '/board'
+
+    txt = ''
+    if dic['problemId'] == '':
+        txt = 'コンテスト全体に関する質問スレッドが追加されました！'
+    else:
+        txt = '{}問題に関する質問スレッドが追加されました！'.format(dic['problemId'])
+
+    txt += '確認してください。\n\n'
+    txt += 'userid: {}'.format(dic['userId']) + '\n'
+    txt += 'policy: {}'.format(dic['policy']) + '\n'
+    txt += 'type: {}'.format(dic['type']) + '\n'
+    txt += 'title: {}'.format(dic['title']) + '\n\n'
+    txt += url
+
+    return {'text': txt}
+
+
+def make_text_about_comment(dic, pid, title):
+    url = 'https://onlinejudge.u-aizu.ac.jp/beta/room.html#' + config.ARENA_ID + '/board'
+
+    txt = ''
+    if pid == '':
+        txt = 'コンテスト全体に関する質問スレッド'
+    else:
+        txt = '{}問題に関する質問スレッド'
+    txt += '「{}」'.format(title)
+    txt += 'に新しいコメントがつきました！確認してください。\n\n'
+
+    txt += url
+
+    return {'text': txt}
+
+
+def checker(now_threads):
+    def check(c):
+        new_problem_dict = get_messages_on_thread(config.ARENA_ID, c)
+        new_threads = new_problem_dict['threads']
+        for new_thread in new_threads:
+            is_exist = False
+            for now_thread in now_threads[c]:
+                if new_thread['id'] == now_thread['id']:
+                    # 新しいコメントが増えてるかどうかチェック
+                    now_comments = now_thread['rootComment']
+                    new_comments = new_thread['rootComment']
+                    for new_cm in new_comments:
+                        flag = False
+                        for now_cm in now_comments:
+                            if new_cm['id'] == now_cm['id']:
+                                flag = True
+                                break
+                        if flag == False:
+                            m = make_text_about_comment(
+                                new_cm, c, new_thread['title'])
+                            requests.post(url=config.SLACK_HOOK_URL, json=m)
+                    is_exist = True
+                    break
+            if is_exist == False:
+                m = make_text_about_thread(new_thread)
+                requests.post(url=config.SLACK_HOOK_URL, json=m)
+        now_threads[c] = new_threads
+    # スレッドが増えたかどうかをチェック
+    for c in string.ascii_uppercase:
+        check(c)
+    check('')
 
 
 def main():
     login(config.AOJ_ID, config.AOJ_PASS)
-    now = []
+
+    now_threads = {}
+
+    for c in string.ascii_uppercase:
+        now_threads[c] = list()
+
+    now_threads[''] = list()
+
     while True:
-        thread_list = get_thread_list(config.ARENA_ID, 1, 20)
-        for dic in thread_list:
-            if dic in now:
-                continue
-            messages = make_text(dic)
-            requests.post(url=config.SLACK_HOOK_URL, json=messages)
-        now = thread_list
+        print('update')
+        checker(now_threads)
         time.sleep(config.SLEEP_TIME)
 
 
